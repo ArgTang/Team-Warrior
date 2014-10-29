93605d828c877922add5bd80104a93ab77c03892
aleksanderaleksic <aleksander.aleksic@hotmail.no>
2014-10-29 19:18:09 +0100

    Luke onclick

---
+++ main.js
@@ -0,0 +1,13 @@
+function åpne(i){
+    
+        var myElement = document.getElementById("dato"+i);
+        if (myElement.className == "dato")
+        {
+            myElement.className = "opendato";   
+        }else{
+            myElement.className = "dato";   
+        }
+}
+function spørrevindu(){
+        
+}
