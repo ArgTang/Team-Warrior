8b04d64e9df51cb58c4b7a4b65f38fa9c9770626
ArgTang <ArgTang@outlook.com>
2014-10-31 14:11:58 +0100

    inputvalidering og forsøk på lagring/henting av brukernavn må testes når skjema er oppe å går

--- main.js
+++ main.js
@@ -9,5 +9,31 @@ function åpne(i){
 }
 
 function spørrevindu(){
+    var bruker = hentebruker();
+    
         
+}
+
+
+// info om localstorage http://diveintohtml5.info/storage.html
+function lagrebruker(studentnummer){
+    if (!supportsLocalStorage()) { return -1; }//sjekker om localstorage metode er tiljengelig
+    if ( b = inputvalidering(studentnummer)){
+        localStorage[deltaker]=studentnummer;
+        return true;
+    }else
+        return false;
+}
+
+function hentebruker(){
+    if (!supportsLocalStorage()) { return false; }
+    studentnummer = parseInt(localstorage[deltaker]);    
+    return ('s'+studentnummer);
+}
+
+function inputvalidering(a){
+    if(a == ""){return false} //sjekker om det er fylt inn noe
+    if (String.indexOf('s') != -1)
+        a = String.slice(1); //fjerner s
+    return (isNaN(a)) //return hvis nummer  
 }
