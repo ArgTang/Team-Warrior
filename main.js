50fd8904c62cd28b15fcf84081f51808be7fe641
ArgTang <ArgTang@outlook.com>
2014-11-02 00:40:30 +0100

    W3CValidation
    Html ->ok
    css -> mangler noen prefix, men hvis jeg sjekker caniuse er de ok. sjekke nav li display inline + width funker ikke sammen?
    javascript -> et ikke om localstorage, men den skal være ok. kan uansett ikke teste funksjonalitet enda."use strict" ska egentlig brukes i hver funksjon, ikke som global. snow.js kan struktureres anderledes, men funker

--- main.js
+++ main.js
@@ -1,41 +1,47 @@
-function åpne(i){
-    
-        var myElement = document.getElementById("dato"+i);
-    
-        if (myElement.className == "dato")
-            myElement.className = "opendato";   
-        else
-            myElement.className = "dato";   
+"use strict"; //compile all js in strict mode
+
+function apne(i) {
+    var myElement = document.getElementById("dato" + i);
+    if (myElement.className === "dato") {
+        myElement.className = "opendato";
+    } else {
+        myElement.className = "dato";
+    }
 }
 
-function spørrevindu(){
-    var bruker = hentebruker();
+function sporrevindu() {
     
-        
 }
-
 
 // info om localstorage http://diveintohtml5.info/storage.html
-function lagrebruker(studentnummer){
-    if (!supportsLocalStorage()) { return -1; }//sjekker om localstorage metode er tiljengelig
-    if (inputvalidering(studentnummer)){
-        localStorage[deltaker]=studentnummer;
+function supportsLocalStorage() {
+    return 'localStorage' in window && window['localStorage'] !== null;
+}
+
+function inputvalidering(a) {
+    if (a === "") { return false; } //sjekker om det er fylt inn noe
+    if (String.indexOf('s') !== -1) {
+        a = String.slice(1);
+    } //fjerner s
+    return (isNaN(a)); //return hvis nummer  
+}
+
+function lagrebruker(studentnummer) {
+    if (!supportsLocalStorage()) { return -1; } //sjekker om localstorage metode er tiljengelig
+    if (inputvalidering(studentnummer)) {
+        localStorage['deltaker'] = studentnummer;
         return true;
-    }else
+    } else {
         return false;
+    }
 }
 
-function hentebruker(){
+function hentebruker() {
     if (!supportsLocalStorage()) { return false; }
-    studentnummer = parseInt(localstorage[deltaker]);
+    var studentnummer = parseInt(localstorage['deltaker'], 10);
     //sjekker om nummer finnes i minne && dobbeltsjekk om det er et nummer
-    if (studentnummer != null && isNaN(studentnummer))
-        return ('s'+studentnummer);
+    if (studentnummer !== null && isNaN(studentnummer)) {
+        return ('s' + studentnummer);
+    }
 }
 
-function inputvalidering(a){
-    if(a == ""){return false} //sjekker om det er fylt inn noe
-    if (String.indexOf('s') != -1)
-        a = String.slice(1); //fjerner s
-    return (isNaN(a)) //return hvis nummer  
-}
