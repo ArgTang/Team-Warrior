76e41acfb6ff1b6a72f7f4fa3509a836694cc411
ArgTang <ArgTang@outlook.com>
2014-11-03 22:52:26 +0100

    W3C

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 "use strict"; //compile all js in strict mode
 
 function apne(i) {
-	var d = new Date();
-    var n = d.getDate();
-    var m = d.getMonth();
+	var d = new Date(),
+        n = d.getDate(),
+        m = d.getMonth(),
+        flipper = document.getElementById("flipper" + i);
     
-    var flipper = document.getElementById("flipper" + i);
-    
-    if (flipper.className === "flipper" && i <= n && m === 10) 
-	{
+    if (flipper.className === "flipper" && i <= n && m === 10) {
         flipper.className = "flipperopen";
-    } 
-	else 
-	{
+    } else {
         flipper.className = "flipper";
     }
 }
@@ -21,7 +17,7 @@ function apne(i) {
 
 // info om localstorage http://diveintohtml5.info/storage.html
 function supportsLocalStorage() {
-    return typeof(Storage) !== "undefined";
+    return typeof (Storage) !== "undefined";
 }
 
 function inputvalidering(a) {
@@ -66,54 +62,54 @@ function sendskjema() {
     //sjekker om lagring suksessfull eller input er godkjent hvis cookies er slått av
     if (temp || (temp === -1 && inputvalidering(input))) {
         
-        if (Document.getElementsName("alt1").innerHTML == true) {
+        if (document.getElementsName("alt1").innerHTML === true) {
             svar[0] = 1;
-        } else if(Document.getElementsName("alt2").innerHTML == true) {
+        } else if (document.getElementsName("alt2").innerHTML === true) {
             svar[0] = 2;
-        }else if(Document.getElementsName("alt3").innerHTML == true) {
+        } else if (document.getElementsName("alt3").innerHTML === true) {
             svar[0] = 3;
-        }else if(Document.getElementsName("alt4").innerHTML == true) {
+        } else if (document.getElementsName("alt4").innerHTML === true) {
             svar[0] = 4;
         }
         
-        if (Document.getElementsName("alt5").innerHTML == true) {
+        if (document.getElementsName("alt5").innerHTML === true) {
             svar[1] = 1;
-        } else if(Document.getElementsName("alt6").innerHTML == true) {
+        } else if (document.getElementsName("alt6").innerHTML === true) {
             svar[1] = 2;
-        }else if(Document.getElementsName("alt7").innerHTML == true) {
+        } else if (document.getElementsName("alt7").innerHTML === true) {
             svar[1] = 3;
-        }else if(Document.getElementsName("alt8").innerHTML == true) {
+        } else if (document.getElementsName("alt8").innerHTML === true) {
             svar[1] = 4;
         }
         
-        if (svar[0] > 0 && svar[1] > 0){
+        if (svar[0] > 0 && svar[1] > 0) {
             SendSvar(temp, svar);
         } else {
-        melding = "Vennligst sjekk at du har krysset av et ssvar på begge spørsmål";
+            melding = "Vennligst sjekk at du har krysset av et ssvar på begge spørsmål";
         }
     } else {
         melding = "Vennligst sjekk at du har skrevet inn ditt studentnummer";
     }
     
-    if (melding != "") {
-        Document.getElementsByClassName("spmmelding").innerHTML = melding;
+    if (melding !== "") {
+        document.getElementsByClassName("spmmelding").innerHTML = melding;
     }
 }
 
 function sporrevindu(dag) {
     fyllinnbruker();
-    var spm = getSpørsmål(dag);
-    if (spm != null) {
-        Document.getElementsByClassName("sporsmal1").innerHTML = spm[0];
-        Document.getElementsName("alt1").innerHTML = spm[1];
-        Document.getElementsName("alt2").innerHTML = spm[2];
-        Document.getElementsName("alt3").innerHTML = spm[3];
-        Document.getElementsName("alt4").innerHTML = spm[4];
-        Document.getElementsByClassName("sporsmal2").innerHTML = spm[5];
-        Document.getElementsName("alt5").innerHTML = spm[6];
-        Document.getElementsName("alt6").innerHTML = spm[7];
-        Document.getElementsName("alt7").innerHTML = spm[8];
-        Document.getElementsName("alt8").innerHTML = spm[9];    
+    var spm = getSporsmal(dag);
+    if (spm !== null) {
+        document.getElementsByClassName("sporsmal1").innerHTML = spm[0];
+        document.getElementsName("alt1").innerHTML = spm[1];
+        document.getElementsName("alt2").innerHTML = spm[2];
+        document.getElementsName("alt3").innerHTML = spm[3];
+        document.getElementsName("alt4").innerHTML = spm[4];
+        document.getElementsByClassName("sporsmal2").innerHTML = spm[5];
+        document.getElementsName("alt5").innerHTML = spm[6];
+        document.getElementsName("alt6").innerHTML = spm[7];
+        document.getElementsName("alt7").innerHTML = spm[8];
+        document.getElementsName("alt8").innerHTML = spm[9];
     }
 }
 
